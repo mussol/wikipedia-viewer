bd6e36ff2351f9a076cdd2c5ade1f02bee52f844
mussol <gemma.valls@gmail.com>
2017-09-15 11:06:44 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,41 @@
+var input = document.querySelector('input');
+var search = document.querySelector('button');
+
+search.addEventListener('click', function() {
+	if (input.value) {
+		console.log('just clicked on search button');
+		searchWikipedia(input.value);
+	}
+});
+
+input.addEventListener('keypress', function(e) {
+	if (e.keyCode === 13 && e.value) {
+		console.log('just pressed enter key with valid search query');
+		searchWikipedia(e.value);
+	};
+});
+
+function searchWikipedia(query) {
+	var request = new XMLHttpRequest();
+	var url = `https://en.wikipedia.org/w/api.php?format=json&action=query&origin=*&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=${query}`;
+	request.open('GET', url, true);
+
+	request.onload = function() {
+	  if (request.status >= 200 && request.status < 400) {
+	    // Success!
+	    var resp = request.responseText;
+	  	console.log('success! resp: ', resp);
+	  } else {
+	    // We reached our target server, but it returned an error
+	    console.log('error!');
+	  }
+	};
+
+	request.onerror = function() {
+	  // There was a connection error of some sort
+	  console.log('connection error!');
+	};
+
+	request.send();
+}
+
